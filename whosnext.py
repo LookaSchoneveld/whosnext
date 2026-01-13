9818033d7f61ace4dd816ba32a2c8a3d66ba262d
LookaSchoneveld <l.b.schoneveld@gmail.com>
2026-01-13 12:54:37 +0100

    added presenters

--- whosnext.py
+++ whosnext.py
@@ -29,7 +29,7 @@ _ = input(figlet_format('ready?', font='starwars', width=500))
 
 ROULETTE = True
 
-presenters_per_meeting = 1
+presenters_per_meeting = 2
 
 current_mc = 'Looka Schoneveld'
 current_members = {
